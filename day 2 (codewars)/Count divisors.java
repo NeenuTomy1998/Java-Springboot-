2b707482a03b3ec32abca1bd1165ee05c7ae13f4
Neenu Tomy <neenut1998@gmail.com>
2021-11-25 13:16:05 +0530

    updated day 2

---
+++ day 2 (codewars)/Count divisors.java
@@ -0,0 +1,15 @@
+public class FindDivisor {
+
+  public long numberOfDivisors(int n) {
+
+     long counter = 0;
+      for(int i=1; i<=n; i++){
+          if(n % i == 0){
+          counter++;
+          }
+      }
+      return counter;
+  }
+
+
+  }
