2bfa54eea556895647a0594f50530b357cd60058
Neenu Tomy <neenut1998@gmail.com>
2021-11-27 21:36:13 +0530

    Interface-Abstraction-Lambda Expression Implemented

---
+++ day 5- Abstraction , Interface, Lambda Expression/LambdaExpression/src/Lambda.java
@@ -0,0 +1,25 @@
+package Java;
+interface Addition{
+    int add(int num1,int num2);
+}
+interface Subtract{
+    int sub(int num1,int num2);
+}
+interface Multiplication{
+    int multiply(int num1,int num2);
+}
+class Main{
+    public static void main(String[] args) {
+        Addition addition=(int num1,int num2) ->{return (num1+num2);};
+        Subtract subtract=(int num1,int num2) ->{return (num1-num2);};
+        Multiplication multiplication=(int num1,int num2) ->{return (num1*num2);};
+        System.out.print("Addition :  ");
+        System.out.println(addition.add(2,2));
+        System.out.print("Subtraction :  ");
+        System.out.println(subtract.sub(100,50));
+        System.out.print("Multiplication :  ");
+        System.out.println(multiplication.multiply(10,10));
+
+
+    }
+}
