8813fd19019696b1aa75ffea39e38fc5195fb0b6
Neenu Tomy <90536190+NeenuTomy1998@users.noreply.github.com>
2021-12-27 22:16:18 +0530

    ABCBanking - MVC updated

---
+++ SpringBoot/27-12-2021- ABCBanking-MVC/ABCBanking- MVC/src/test/java/com/example/ABCBanking/AbcBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ABCBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
